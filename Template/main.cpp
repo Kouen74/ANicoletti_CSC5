c0f142e8c660458b299427fe85b187c4bdcf532f
Kouen74 <grenadesnham@gmail.com>
2016-06-22 14:48:31 -0700

    Signed-off-by: Kouen74 <grenadesnham@gmail.com>

---
+++ Template/main.cpp
@@ -0,0 +1,32 @@
+
+/* 
+ * File:   Template.cpp
+ * Author: Andrew Nicoletti
+ * Created on June 22, 2016, 1:20 AM
+ * Purpose:
+ */
+ 
+#include <iostream>     // System Library
+
+using namespace std;    // Standard I/O Namespace
+
+// User Libraries 
+
+// Global Constants
+
+// Functions Prototypes 
+
+// Exe begins here
+
+int main(int argc, char** argv) {
+
+    // Declare Variables
+    
+    // Input Data
+    
+    // Process Data
+    
+    // Output Data
+    return 0;
+}
+
