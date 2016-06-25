1649bd89b486773a878ca53a5fe5b715215d507c
Kouen74 <grenadesnham@gmail.com>
2016-06-24 20:37:01 -0700

    Signed-off-by: Kouen74 <grenadesnham@gmail.com>

---
+++ Homework/Hello World/Helloworld.cpp
@@ -0,0 +1,35 @@
+
+/* 
+ * File:   Helloworld.cpp
+ * Author: Andrew Nicoletti
+ * Created on June 24, 2016, 8:50 PM
+ * Purpose: hello world
+ */
+ 
+#include <iostream>     // System Library
+
+using namespace std;    // Standard I/O Namespace
+
+// User Libraries 
+
+// Global Constants
+
+// Functions Prototypes 
+
+// Exe begins here
+
+int main(int argc, char** argv) {
+   
+    // Declare Variables
+  
+    
+    // Input Data
+    
+    // Process Data
+   
+    // Output Data
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
+
