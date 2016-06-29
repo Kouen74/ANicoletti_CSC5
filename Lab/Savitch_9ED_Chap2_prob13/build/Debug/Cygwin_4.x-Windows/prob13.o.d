712d89a5e03c1661e3a4c34b33dd918a83306478
Kouen74 <grenadesnham@gmail.com>
2016-06-29 12:59:12 -0700

    Signed-off-by: Kouen74 <grenadesnham@gmail.com>

---
+++ Lab/Savitch_9ED_Chap2_prob13/build/Debug/Cygwin_4.x-Windows/prob13.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_4.x-Windows/prob13.o: prob13.cpp
--- Lab/Savitch_9ED_Chap2_prob13/prob13.cpp
+++ Lab/Savitch_9ED_Chap2_prob13/prob13.cpp
@@ -1,9 +1,9 @@
 
 /* 
- * File:   template.cpp
+ * File:   prob13.cpp
  * Author: Andrew Nicoletti
- * Created on June 27, 2016, 12:30 PM
- * Purpose: Template
+ * Created on June 28, 2016, 12:30 PM
+ * Purpose: BMR
  */
  
 #include <iostream>     // System Library
@@ -21,6 +21,7 @@ using namespace std;    // Standard I/O Namespace
 int main(int argc, char** argv) {
    
     // Declare Variables
+    char mf;
     float lbs,inches,yrs;
     float bmr,chocBar=230.0f;
     int nBars;
@@ -30,14 +31,19 @@ int main(int argc, char** argv) {
     cout<<"Eat less than this to lose weight:"<<endl;
     cout<<"Input male of female with m or f"<<endl;
     cin>>mf;
-    cout<<"Input weight(lbs), height(inches), age(yrs)"endl;
+    cout<<"Input weight(lbs), height(inches), age(yrs)"<<endl;
     cin>>lbs>>inches>>yrs;
     
     // Process Data
-   
-    
+    if(mf=='m'||mf=='M'){
+        bmr=66+6.3*lbs+12.9*inches+6.8*yrs;
+    }else{
+        bmr=655+4.3*lbs+4.7*inches+4.7*yrs;
+        
+    }
+    nBars=bmr/chocBar+1;
     // Output Data
-   
+    cout<<"Eat less than "<<nBars<<" chocolate bars to lose weight"<<endl;
     
     return 0;
 }
